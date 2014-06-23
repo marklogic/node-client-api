0cb2051d4e47c5072367a389072e797913246668
Erik Hennum <efhennum@gmail.com>
2014-06-23 14:42:49 -0700

    rest server properties

---
+++ test/rest-server-properties.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2014 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var should = require('should');
+
+var testutil = require('./test-util.js');
+
+var valcheck = require('core-util-is');
+var marklogic = require('../');
+
+var db = marklogic.createDatabaseClient(testutil.restAdminConnection);
+
+describe('rest server properties', function() {
+  it('should read and write properties', function(done) {
+    var propKey = 'validate-queries';
+    var origVal = null;
+    db.config.properties.read().result().
+    then(function(props) {
+      valcheck.isNullOrUndefined(props).should.equal(false);
+      origVal = props[propKey];
+      valcheck.isNullOrUndefined(origVal).should.equal(false);
+      props[propKey] = !origVal;
+      return db.config.properties.write(props).result();
+      }).
+    then(function(response) {
+      return db.config.properties.read().result();
+      }).
+    then(function(props) {
+      valcheck.isNullOrUndefined(props).should.equal(false);
+      var modVal = props[propKey];
+      valcheck.isNullOrUndefined(modVal).should.equal(false);
+      modVal.should.equal(!origVal);
+      var sparseProps = {};
+      sparseProps[propKey] = origVal;
+      return db.config.properties.write(sparseProps).result();
+      }).
+    then(function(response) {
+      return db.config.properties.read().result();
+      }).
+    then(function(props) {
+      valcheck.isNullOrUndefined(props).should.equal(false);
+      var revertVal = props[propKey];
+      valcheck.isNullOrUndefined(revertVal).should.equal(false);
+      revertVal.should.equal(origVal);
+      done();
+    }, done);
+  });
+});
