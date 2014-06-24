13437489c52059579f313cd87df75e962d9207c5
Erik Hennum <efhennum@gmail.com>
2014-06-24 11:33:45 -0700

    content version for bulk read

--- test/documents-version.js
+++ test/documents-version.js
@@ -27,31 +27,30 @@ var db = marklogic.createDatabaseClient(testutil.restWriterConnection);
 var dbAdmin = marklogic.createDatabaseClient(testutil.restAdminConnection);
 
 describe('document versions', function() {
-  var originalUpdatePolicy = null;
   before(function(done) {
-    dbAdmin.config.properties.read().result(function(properties) {
-      originalUpdatePolicy = properties['update-policy'];
-      dbAdmin.config.properties.write({'update-policy':'version-required'}).
-      result(function(response) {
-        done();
-      }, done);
-    }, done);
+    dbAdmin.config.properties.write({'update-policy':'version-required'}).
+    result(function(response) {done();}, done);
   });
   after(function(done) {
-    dbAdmin.config.properties.write({'update-policy':originalUpdatePolicy}).
-    result(function(response) {
-      done();
-    }, done);
+    dbAdmin.config.properties.write({'update-policy':'merge-metadata'}).
+    result(function(response) {done();}, done);
   });
   describe('check', function() {
     var uri = '/test/version/check1.json';
     before(function(done){
-      db.documents.write({
-        uri: uri,
-        contentType: 'application/json',
-        content: {key1: 'value 1'}
-        }).
-        result(function(response){done();}, done);
+      db.check(uri).result().
+      then(function(response) {
+        if (response.exists === false) {
+          db.documents.write({
+            uri: uri,
+            contentType: 'application/json',
+            content: {key1: 'value 1'}
+            }).
+            result(function(response){done();}, done);
+        } else {
+          done();
+        }
+      }, done);
     });
     it('should return the versionId', function(done) {
       db.check(uri).
@@ -66,20 +65,27 @@ describe('document versions', function() {
     var uri       = '/test/version/del1.json';
     var versionId = null;
     before(function(done){
-      db.documents.write({
-        uri: uri,
-        contentType: 'application/json',
-        content: {key1: 'value 1'}
-        }).result().
-      then(function(response){
-          return db.check(uri).result();
-        }).
-      then(function(response){
-        versionId = response.versionId;
-        done();
-        }, done);
+      db.check(uri).result(function(response){
+        if (response.exists === true) {
+          versionId = response.versionId;
+          done();
+        } else {
+          db.documents.write({
+            uri: uri,
+            contentType: 'application/json',
+            content: {key1: 'value 1'}
+            }).result().
+          then(function(response){
+              return db.check(uri).result();
+            }).
+          then(function(response){
+            versionId = response.versionId;
+            done();
+            }, done);
+        }
+      }, done);
     });
-    it('should fail to delete without a versionId', function(done) {
+    it('should fail without a versionId', function(done) {
       db.documents.remove(uri).
       result(function(response){
         response.should.equal('SHOULD HAVE FAILED');
@@ -89,7 +95,7 @@ describe('document versions', function() {
         done();
       });
     });
-    it('should fail to delete with the wrong versionId', function(done) {
+    it('should fail with the wrong versionId', function(done) {
       db.documents.remove({uri: uri, versionId: 1234567890}).
       result(function(response){
         response.should.equal('SHOULD HAVE FAILED');
@@ -99,7 +105,7 @@ describe('document versions', function() {
         done();
       });
     });
-    it('should delete with the rightversionId', function(done) {
+    it('should succeed with the rightversionId', function(done) {
       db.documents.remove({uri: uri, versionId: versionId}).
       result(function(response){
         valcheck.isUndefined(response).should.equal(false);
@@ -107,16 +113,131 @@ describe('document versions', function() {
       }, done);
     });
   });
-  describe('read', function() {
+  describe('overwrite', function() {
+    var overwriteUri = '/test/version/write1.json';
+    var versionId    = null;
+    before(function(done){
+      db.check(overwriteUri).result(function(response){
+        if (response.exists === true) {
+          versionId = response.versionId;
+          done();
+        } else {
+          db.documents.write({
+            uri: overwriteUri,
+            contentType: 'application/json',
+            content: {key1: 'value 1'}
+            }).result().
+          then(function(response){
+              return db.check(overwriteUri).result();
+            }).
+          then(function(response){
+            versionId = response.versionId;
+            done();
+            }, done);
+        }
+      }, done);
+    });
+    it('should fail without a versionId', function(done) {
+      db.documents.write({
+        uri: overwriteUri,
+        contentType: 'application/json',
+        content: {key1: 'no version'}
+        }).
+      result(function(response){
+        response.should.equal('SHOULD HAVE FAILED');
+        done();
+      }, function(error) {
+        (error.indexOf('RESTAPI-CONTENTNOVERSION') > -1).should.equal(true);
+        done();
+      });
+    });
+    it('should fail with the wrong versionId', function(done) {
+      db.documents.write({
+        uri: overwriteUri,
+        versionId: 1234567890,
+        contentType: 'application/json',
+        content: {key1: 'wrong version'}
+        }).
+      result(function(response){
+        response.should.equal('SHOULD HAVE FAILED');
+        done();
+      }, function(error) {
+        (error.indexOf('RESTAPI-CONTENTWRONGVERSION') > -1).should.equal(true);
+        done();
+      });
+    });
+    it('should succeed with the right versionId', function(done) {
+      db.documents.write({
+        uri: overwriteUri,
+        versionId: versionId,
+        contentType: 'application/json',
+        content: {key1: 'version '+versionId}
+        }).
+      result(function(response){
+        valcheck.isUndefined(response).should.equal(false);
+        done();
+      }, done);
+    });
   });
-  describe('write', function() {
+  describe('create', function() {
+    var createUri = '/test/version/create1.json';
+    before(function(done){
+      db.check(createUri).result(function(response){
+        if (response.exists === true) {
+          db.remove({uri: createUri, versionId: response.versionId}).
+          result(function(response) {done();}, done);
+        } else {
+          done();
+        }
+      }, done);
+    });
+    after(function(done) {
+      db.check(createUri).result(function(response){
+        db.remove({uri: createUri, versionId: response.versionId}).
+        result(function(response) {done();}, done);
+      });
+    });
+    it('should succeed with no version', function(done) {
+      db.documents.write({
+        uri: createUri,
+        contentType: 'application/json',
+        content: {key1: 'create with no version'}
+        }).
+      result(function(response){
+        valcheck.isUndefined(response).should.equal(false);
+        done();
+      }, done);
+    });
+  });
+  describe('read', function() {
+    var uri = '/test/version/read1.json';
+    before(function(done){
+      db.check(uri).result().
+      then(function(response) {
+        if (response.exists === false) {
+          db.documents.write({
+            uri: uri,
+            contentType: 'application/json',
+            content: {key1: 'value 1'}
+            }).
+            result(function(response){done();}, done);
+        } else {
+          done();
+        }
+      }, done);
+    });
+    it('should return the versionId', function(done) {
+      db.read(uri).
+      result(function(documents) {
+        valcheck.isArray(documents).should.equal(true);
+        documents.length.should.equal(1);
+        var document = documents[0];
+        valcheck.isUndefined(document.versionId).should.equal(false);
+        done();
+        }, done);
+    });
   });
 /* TODO:
-    read without versionId should return versionId with content
-    read with wrong versionId should return versionId with content 
     read with right versionId should return no content
-    write of new without versionId should succeed
-    write of existing without versionId should fail
-    write of existing with wrong versionId should fail
  */
 });
