88306eec3b61c3e0e38f6b1cc922a5ac402c83be
Aries Yuwono <ayuwono@ayuwono-z620.marklogic.com>
2014-11-05 14:24:07 -0800

    add test

--- test-complete/nodejs-config-query-negative.js
+++ test-complete/nodejs-config-query-negative.js
@@ -34,20 +34,21 @@ describe('Config query test', function(){
   var dbPath = dbDir + dbModule;
   var fsPath = './test-complete/data/directoryConstraintInvalid.xqy';
 
-  /*it('should write the custom query', function(done){
+  it('should fail to write the custom query with reader user', function(done){
     this.timeout(3000);
-    dbAdmin.config.query.custom.write(
-      dbModule, 
+    db.config.query.custom.write(
+      'directoryConstraint.xqy', 
       [{'role-name':'app-user', capabilities:['execute']}], 
-      fs.createReadStream(fsPath)).
+      fs.createReadStream('./test-complete/data/directoryConstraint.xqy')).
     result(function(response) {
       response.should.equal('SHOULD HAVE FAILED');
       done();
     }, function(error) {
-      console.log(error);
+      //console.log(error);
+      error.statusCode.should.equal(403);
       done();
     });
-  });*/
+  });
 
   it('should fail to read the invalid custom query', function(done){
     dbAdmin.config.query.custom.read(dbModule).
@@ -92,90 +93,5 @@ describe('Config query test', function(){
     });
   });
     
-  /*it('should list the custom query', function(done){
-    dbAdmin.config.query.custom.list().
-    result(function(response){
-      //console.log(response);
-      response.should.have.property('assets');
-      response.assets.length.should.be.greaterThan(0);
-      response.assets[0].asset.should.equal('/ext/marklogic/query/custom/directoryConstraint.xqy');
-      done();
-    }, done);
-  });
-
-  it('should remove the custom query', function(done){
-    dbAdmin.config.query.custom.remove(dbModule).
-    result(function(response){
-      //console.log(response);
-      response.path.should.equal('/marklogic/query/custom/directoryConstraint.xqy');
-      done();
-    }, done);
-  });
-
-  it('should list 0 custom query', function(done){
-    dbAdmin.config.query.custom.list().
-    result(function(response){
-      //console.log(response);
-      response.assets.length.should.equal(0);
-      done();
-    }, done);
-  });
-
-  it('should write the custom query for test', function(done){
-    this.timeout(3000);
-    dbAdmin.config.query.custom.write(
-      dbModule, 
-      [{'role-name':'app-user', capabilities:['execute']}], 
-      fs.createReadStream(fsPath)).
-    result(function(response){
-      done();
-    }, done);
-  });
-
-  it('should write document for test', function(done){
-    dbWriter.documents.write({
-      uri: '/test/custom/query/customQuery1.json', 
-      collections: ['customCollection'], 
-      contentType: 'application/json',
-      content: {
-        title: 'this is custom query'
-      }
-    }).
-    result(function(response) {
-      //console.log(response);
-      done();
-    }, done);
-  });
-
-  it('should use custom query in a query', function(done){
-    dbWriter.documents.query(
-      q.where(
-        q.parsedFrom('dirs:/test/custom/query/',
-          q.parseBindings(
-            q.parseFunction('directoryConstraint.xqy', q.bind('dirs'))
-          )
-        )
-      )
-    ).
-    result(function(response) {
-      //console.log(response);
-      response[0].uri.should.equal('/test/custom/query/customQuery1.json');
-      done();
-    }, done);
-  });
-
-  it('should remove the custom query after the query test', function(done){
-    dbAdmin.config.query.custom.remove(dbModule).
-    result(function(response){
-      done();
-    }, done);
-  });
-
-  it('should remove the document', function(done){
-    dbAdmin.documents.removeAll({directory: '/test/custom/query/'}).
-    result(function(response){
-      done();
-    }, done);
-  });*/
 
 });
