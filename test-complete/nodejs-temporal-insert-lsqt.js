b2642f827254065a26df8fb932f3afb14f94cb70
gvaidees <gvaidees@marklogic.com>
2014-11-09 21:15:06 -0800

    Temporal functionality testing

---
+++ test-complete/nodejs-temporal-insert-lsqt.js
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2014 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var should = require('should');
+
+var testlib    = require('../etc/test-lib.js');
+var testconfig = require('../etc/test-config.js');
+
+var marklogic = require('../');
+
+var concatStream = require('concat-stream');
+var fs = require('fs');
+
+var temporalCollectionName = 'temporalCollectionLsqt';
+
+testconfig.manageAdminConnection.user     = "admin";
+testconfig.manageAdminConnection.password = "admin";
+var adminClient = marklogic.createDatabaseClient(testconfig.manageAdminConnection);
+var adminManager = testlib.createManager(adminClient);
+var db = marklogic.createDatabaseClient(testconfig.restWriterConnection);
+var dbReader = marklogic.createDatabaseClient(testconfig.restReaderConnection);
+
+describe('Write Document Test', function() {
+  
+  var docuri = 'temporalDoc.json'; 
+
+  before(function(done) {
+    this.timeout(3000);
+    db.documents.write({
+      uri: docuri,
+      collections: ['coll0', 'coll1'],
+      temporalCollection: temporalCollectionName,
+      contentType: 'application/json',
+      quality: 10,
+      permissions: [
+        {'role-name':'app-user', capabilities:['read']},
+        {'role-name':'app-builder', capabilities:['read', 'update']}
+      ],
+      properties: {prop1:'foo', prop2:25},
+      content: {
+        'System': {
+          'systemStartTime' : "1999-01-01T00:00:00",  // THis value should not matter
+          'systemEndTime' : "",
+        },
+        'Valid': {
+          'validStartTime': "2001-01-01T00:00:00",
+          'validEndTime': "2011-12-31T23:59:59"
+        },
+        'Address': "999 Skyway Park",
+        'uri': "javaSingleDoc1.json",
+        id: 12, 
+        name: 'Jason'
+      },
+      systemTime: '2010-01-01T00:00:00'
+    }
+    ).result(function(response){done();}, done);
+  });
+
+  it('should read the document content name', function(done) {
+    db.documents.read({uris: docuri}).result(function(documents) {
+      var document = documents[0];
+      document.content.Address.should.equal('999 Skyway Park');
+      done();
+    }, done);
+  });
+
+  it('should read the document valid dates', function(done) {
+    db.documents.read({uris: docuri, categories:['content']}).result(function(documents) {
+      var document = documents[0];
+      document.content.System.systemStartTime.should.equal("2010-01-01T00:00:00");
+      document.content.Valid.validStartTime.should.equal("2001-01-01T00:00:00");
+      document.content.Valid.validEndTime.should.equal("2011-12-31T23:59:59");
+      done();
+    }, done);
+  });
+
+  it('should read the document quality (metadata) and content', function(done) {
+    db.documents.read({uris: docuri, categories:['metadata', 'content']})
+    .result(function(documents) {
+      var document = documents[0];
+      document.quality.should.equal(10);
+      document.properties.prop1.should.equal('foo');
+      done();
+    }, done);
+  });
+
+  it('should read the document collections', function(done) {
+    db.documents.read({uris: docuri, categories:['metadata']}).result(function(documents) {
+      var document = documents[0];
+      var collCount = document.collections.length;
+      collCount.should.equal(5);  // Should be coll0, coll1, temporalCollectionLsqt, docUri and latest
+      for (var i=0; i < collCount; i++) {
+        var coll = document.collections[i];
+
+        if (document.collections[i] !== temporalCollectionName && document.collections[i] !== 'coll0' &&
+            document.collections[i] !== 'coll1' && document.collections[i] !== 'latest' &&
+            document.collections[i] !== docuri) {
+          console.log("Invalid Collection: " + coll);
+          should.equal(false, true);
+        }
+      }      
+      done();
+    }, done);
+  });
+
+  it('should read the document permissions', function(done) {
+    db.documents.read({uris: docuri, categories:['metadata']}).result(function(documents) {
+      var document = documents[0];
+      var permissionsCount = 0;
+      document.permissions.forEach(function(permission) {
+        switch(permission['role-name']) {
+          case 'app-user':
+            permissionsCount++;
+            permission.capabilities.length.should.equal(1);
+            permission.capabilities[0].should.equal('read');
+            break;
+          case 'app-builder':
+            permissionsCount++;
+            permission.capabilities.length.should.equal(2);
+            permission.capabilities.should.containEql('read');
+            permission.capabilities.should.containEql('update');
+            break;
+        }
+      });
+      permissionsCount.should.equal(2);
+      done();
+    }, done);
+  });
+
+  it('should read multiple documents', function(done) {
+    db.documents.read({uris: [docuri], categories:['content']}).result(function(documents) {
+      documents[0].content.id.should.equal(12);
+      done();
+    }, done);
+  });
+
+  it('should read multiple documents with an invalid one', function(done) {
+    db.documents.read({uris: [docuri, '/not/here/blah.json'], categories:['content']}).result(function(documents) {
+      documents[0].content.id.should.equal(12);
+      done();
+    }, done);
+  });
+
+
+  after(function(done) {
+   return adminManager.post({
+      endpoint: '/manage/v2/databases/' + testconfig.testServerName,
+      contentType: 'application/json',
+      accept: 'application/json',
+      body:   {'operation': 'clear-database'}
+    }).result().then(function(response) {
+      console.log("Before setting LSQT in response");
+
+      return adminManager.put({
+        endpoint: '/manage/v2/databases/'+testconfig.testServerName+'/temporal/collections/lsqt/properties?collection=temporalCollectionLsqt',
+        body: {
+          "lsqt-enabled": true
+        }
+      }).result(), done();
+    }, function(err) {
+      console.log("Before setting LSQT in error");
+
+      adminManager.put({
+        endpoint: '/manage/v2/databases/'+testconfig.testServerName+'/temporal/collections/lsqt/properties?collection=temporalCollectionLsqt',
+        body: {
+          "lsqt-enabled": true
+        }
+      }).result();
+      done();
+    },
+    done);
+  });
+
+});
---
+++ test-complete/nodejs-temporal-patch.js
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2014 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var should = require('should');
+
+var testconfig = require('../etc/test-config.js');
+var testlib    = require('../etc/test-lib.js');
+
+var marklogic = require('../');
+
+testconfig.manageAdminConnection.user     = "admin";
+testconfig.manageAdminConnection.password = "admin";
+var adminClient = marklogic.createDatabaseClient(testconfig.manageAdminConnection);
+var adminManager = testlib.createManager(adminClient);
+
+var db = marklogic.createDatabaseClient(testconfig.restWriterConnection);
+var dbReader = marklogic.createDatabaseClient(testconfig.restReaderConnection);
+var dbWriter = marklogic.createDatabaseClient(testconfig.restWriterConnection);
+var q = marklogic.queryBuilder;
+var p = marklogic.patchBuilder;
+
+describe('Write Document Test', function() {
+  
+  var docuri = 'temporalDoc.json'; 
+ 
+  before(function(done) {
+    this.timeout(3000);
+    dbWriter.documents.write({
+      uri: docuri,
+      collections: ['coll0', 'coll1'],
+      temporalCollection: 'temporalCollection',
+      contentType: 'application/json',
+      quality: 10,
+      permissions: [
+        {'role-name':'app-user', capabilities:['read']},
+        {'role-name':'app-builder', capabilities:['read', 'update', 'execute']}
+      ],
+      properties: {prop1:'foo updated', prop2:50},
+      content: {
+        'System': {
+          'systemStartTime' : "",
+          'systemEndTime' : "",
+        },
+        'Valid': {
+          'validStartTime': "2003-01-01T00:00:00",
+          'validEndTime': "2008-12-31T23:59:59"
+        },
+        'Address': "888 Skyway Park",
+        'uri': "javaSingleDoc1.json",
+        id: 12, 
+        name: 'Jason'
+      }
+    }).result(function(response){done();}, done);
+  });
+
+  // Cannot set collections on a temporal document
+  it('should not apply the patch with collections', function(done){
+    dbWriter.documents.patch({
+      uri: docuri,
+      categories: ['metadata'],
+      operations: [
+        p.insert('array-node("collections")', 'last-child', 'addedCollection')
+        // p.replace('quality', 24)
+      ]
+    }).result(function(response) {
+        //console.log(JSON.stringify(response, null, 4));
+        response.uri.should.equal(docuri);
+        done();
+    }, function(err) {
+        err.statusCode.should.equal(400);  // Bad request
+        done();
+    },
+    done);
+  });
+
+  // Cannot set quality on a temporal document
+  it('should not apply the patch with quality', function(done){
+    dbWriter.documents.patch({
+      uri: docuri,
+      categories: ['metadata'],
+      operations: [
+        p.replace('quality', 24)
+      ]
+    }).result(function(response) {
+        response.uri.should.equal(docuri);
+        done();
+    }, function(err) {
+        err.statusCode.should.equal(400);  // Bad request
+        done();
+    },
+    done);
+  });
+
+  // Cannot patch content on a temporal document
+  it('should not apply the patch with content', function(done){
+    dbWriter.documents.patch(docuri,
+      p.pathLanguage('jsonpath'),
+      p.replace('$.name', 'Bourne')
+    ).result(function(response) {
+        //console.log(JSON.stringify(response, null, 4));
+        response.uri.should.equal(docuri);
+        done();
+    }, function(err) {
+        err.statusCode.should.equal(400);  // Bad request
+        done();
+    },
+    done);
+  });
+
+  // Properties of a temporal document can be set
+  it('should apply the patch with properties', function(done){
+    dbWriter.documents.patch({
+      uri: docuri,
+      categories: ['metadata'],
+      operations: [
+        p.insert('properties', 'last-child', {'newPropKey': 'Newly Inserted Property'})
+        // p.replace('quality', 24)
+      ]
+    }).result(function(response) {
+        //console.log(JSON.stringify(response, null, 4));
+        response.uri.should.equal(docuri);
+        done();
+    }, done);
+  });
+
+  after(function(done) {
+   return adminManager.post({
+      endpoint: '/manage/v2/databases/' + testconfig.testServerName,
+      contentType: 'application/json',
+      accept: 'application/json',
+      body:   {'operation': 'clear-database'}
+    }).result().then(function(response) {
+      adminManager.put({
+        endpoint: '/manage/v2/databases/'+testconfig.testServerName+'/temporal/collections/lsqt/properties?collection=temporalCollectionLsqt',
+        body: {
+          "lsqt-enabled": true
+        }
+      }).result();
+      done();
+    }, done);
+  });
+
+});
