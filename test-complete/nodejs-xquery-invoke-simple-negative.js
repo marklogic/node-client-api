b3fe9c7d696e899846dee8c50f787e007633f437
Aries Yuwono <ayuwono@ayuwono-z620.marklogic.com>
2014-11-19 14:54:33 -0800

    add

---
+++ test-complete/nodejs-xquery-invoke-simple-negative.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2014 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var should = require('should');
+
+var testconfig = require('../etc/test-config.js');
+var fs = require('fs');
+
+var marklogic = require('../');
+
+var db = marklogic.createDatabaseClient(testconfig.restReaderConnection);
+var dbWriter = marklogic.createDatabaseClient(testconfig.restWriterConnection);
+var dbEval = marklogic.createDatabaseClient(testconfig.restEvaluatorConnection);
+var dbAdmin = marklogic.createDatabaseClient(testconfig.restAdminConnection);
+
+describe('Xquery invoke simple negative test', function(){
+  
+  var fsPath = './test-complete/data/sourceSimpleNegative.xqy';
+  var invokePath = '/ext/invokeTest/sourceSimpleNegative.xqy';
+
+  before(function(done) {
+    this.timeout(3000);
+    dbAdmin.config.extlibs.write({
+      path:invokePath, contentType:'application/xquery', source:fs.createReadStream(fsPath)
+    }).
+    result(function(response){done();}, done);
+  });
+ 
+  after(function(done) {
+    dbAdmin.config.extlibs.remove(invokePath).
+    result(function(response){done();}, done);
+  });
+
+  it('should do simple xquery invoke', function(done){
+    dbEval.invoke(invokePath).result(function(values) {
+      //console.log(values);
+      values.should.equal('SHOULD HAVE FAILED');
+      done();
+    }, function(error) {
+      //console.log(error);
+      error.statusCode.should.equal(500);
+      error.body.errorResponse.messageCode.should.equal('XDMP-UNDVAR');
+      done();
+    });
+  });
+
+});
