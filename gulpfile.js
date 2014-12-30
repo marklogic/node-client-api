d1429504191ff220b3dec8611ad232de021c137e
ehennum <erik.hennum@marklogic.com>
2014-12-29 18:35:12 -0800

    increment copyright in comments per #97

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 MarkLogic Corporation
+ * Copyright 2014-2015 MarkLogic Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -47,7 +47,7 @@ gulp.task('doc', function() {
       {
         path:              './etc/marklogic-template',
         systemName:        'MarkLogic Node.js API',
-        copyright:         'Copyright 2014 MarkLogic Corporation',
+        copyright:         'Copyright 2014-2015 MarkLogic Corporation',
         theme:             'marklogic',
         inverseNav:        true,
         navType:           'vertical',
