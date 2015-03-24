db97cb48bad6b806e0260d1c7124edb58c8bf69c
Aries Yuwono <ayuwono@ayuwono-z620.marklogic.com>
2015-03-24 13:58:01 -0700

    add verification on timeLimit

--- test-complete/nodejs-documents-transaction-combo.js
+++ test-complete/nodejs-documents-transaction-combo.js
@@ -46,8 +46,7 @@ describe('Transaction combo test', function() {
     .then(function(response) {
       //console.log(JSON.stringify(response, null, 2));
       response['transaction-status']['transaction-id'].should.equal(tid);
-      //response['transaction-status']['time-limit'].should.equal(30);
-      //console.log(response['transaction-status']['time-limit']);
+      response['transaction-status']['time-limit'].should.equal('30');
       return db.transactions.rollback(tid).result();
     })
     .then(function(response) {
@@ -58,7 +57,7 @@ describe('Transaction combo test', function() {
     .then(function(response) {
       //console.log(JSON.stringify(response, null, 2));
       response.length.should.equal(0);
-      return db.transactions.open({transactionName: 'testTransaction2', timeLimit: 30}).result();
+      return db.transactions.open({transactionName: 'testTransaction2', timeLimit: 60}).result();
     })
     .then(function(response) {
       //console.log(JSON.stringify(response, null, 2));
@@ -77,7 +76,7 @@ describe('Transaction combo test', function() {
     .then(function(response) {
       //console.log(JSON.stringify(response, null, 2));
       response['transaction-status']['transaction-name'].should.equal('testTransaction2');
-      //console.log(response['transaction-status']['time-limit']);
+      response['transaction-status']['time-limit'].should.equal('60');
       return db.transactions.commit(tid2).result();
     })
     .then(function(response) {
