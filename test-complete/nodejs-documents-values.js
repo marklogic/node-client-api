af34b3b312cb53b494630e821f8000531e16d648
MayankBhatnagar89 <mayank.bhatnagar89@yahoo.com>
2015-07-02 12:32:18 +0530

    Test Fix as per bug 32649

--- test-complete/nodejs-documents-values.js
+++ test-complete/nodejs-documents-values.js
@@ -256,8 +256,8 @@ describe('Document tuples test', function(){
         var strData = JSON.stringify(response);
         //console.log(strData);
         strData.should.containEql('"name":"correlation","_value":"0.263822426505543"');
-        strData.should.containEql('"name":"covariance","_value":"0.35"');
-        done();
+        strData.should.containEql[('"name":"covariance","_value":"0.35"') || ('"name":"covariance","_value":"0.349999999999998"')];
+		ndone();
       }, done);
   });
 
