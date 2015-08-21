fe17067a449db1fd6d2a5c79e49ef0bee6304d69
ehennum <erik.hennum@marklogic.com>
2015-08-21 15:04:25 -0700

    semantics enhancements including sparql update

--- test-basic/graphs.js
+++ test-basic/graphs.js
@@ -29,6 +29,8 @@ var db = marklogic.createDatabaseClient(testconfig.restWriterConnection);
 
 describe('graph operations', function(){
   var graphUri   = 'marklogic.com/people';
+  var graph2Uri  = 'marklogic.com/graph2';
+  var graph3Uri  = 'marklogic.com/graph3';
   var graphPath  = './test-basic/data/people.ttl';
   var sparqlPath = './test-basic/data/people.rq';
   it('should write the default graph', function(done){
@@ -81,13 +83,43 @@ describe('graph operations', function(){
  */
   it('should write a named graph as a stream', function(done){
     this.timeout(3000);
-    var writer = db.graphs.createWriteStream(graphUri, 'text/turtle');
+    var writer = db.graphs.createWriteStream({
+      uri:         graphUri,
+      contentType: 'text/turtle',
+      permissions: [
+          {'role-name':'app-user',    capabilities:['read']},
+          {'role-name':'app-builder', capabilities:['read', 'update']}]
+      });
     writer.result(function(response){
       response.should.have.property('graph');
       response.graph.should.equal(graphUri);
-      done();
+      return db.graphs.read({
+        uri:         graphUri,
+        category:    'permissions',
+        contentType: 'application/json'
+        }).result();
       })
-    .catch(done);
+      .then(function(response){
+        var permissionsFound = 0;
+        response.permissions.forEach(function(permission){
+          switch (permission['role-name']) {
+          case 'app-user':
+            permissionsFound++;
+            permission.capabilities.length.should.equal(1);
+            permission.capabilities[0].should.equal('read');
+            break;
+          case 'app-builder':
+            permissionsFound++;
+            permission.capabilities.length.should.equal(2);
+            permission.capabilities.should.containEql('read');
+            permission.capabilities.should.containEql('update');
+            break;
+          }
+        });
+        permissionsFound.should.equal(2);
+        done();
+      })
+      .catch(done);
     fs.createReadStream(graphPath).pipe(writer);
   });
   it('should read the named graph as a stream', function(done){
@@ -143,6 +175,89 @@ describe('graph operations', function(){
   it('should delete the named graph', function(done){
     db.graphs.remove(graphUri)
     .result(function(response){
+      done();
+      })
+    .catch(done);
+  });
+  it('should run a SPARQL update', function(done){
+    this.timeout(3000);
+    db.graphs.sparqlUpdate({
+        usingNamedGraphs: true,
+        permissions:      [
+          {'role-name':'app-user',    capabilities:['read']},
+          {'role-name':'app-builder', capabilities:['read', 'update']}],
+        data:
+          'INSERT DATA { GRAPH <'+graph2Uri+'> { <marklogic.com/r1> <marklogic.com/p1> "literal1" } }'
+      })
+    .result(function(response){
+      return db.graphs.read({
+        uri:         graph2Uri,
+        category:    'permissions',
+        contentType: 'application/json'
+        }).result();
+      })
+    .then(function(response){
+      var permissionsFound = 0;
+      response.permissions.forEach(function(permission){
+        switch (permission['role-name']) {
+        case 'app-user':
+          permissionsFound++;
+          permission.capabilities.length.should.equal(1);
+          permission.capabilities[0].should.equal('read');
+          break;
+        case 'app-builder':
+          permissionsFound++;
+          permission.capabilities.length.should.equal(2);
+          permission.capabilities.should.containEql('read');
+          permission.capabilities.should.containEql('update');
+          break;
+        }
+      });
+      permissionsFound.should.equal(2);
+      return db.graphs.read(graph2Uri, 'application/rdf+json').result();
+      })
+    .then(function(response){
+      response.should.have.property('marklogic.com/r1');
+      response['marklogic.com/r1'].should.have.property('marklogic.com/p1');
+      response['marklogic.com/r1']['marklogic.com/p1'].length.should.equal(1);
+      response['marklogic.com/r1']['marklogic.com/p1'][0].should.have.property('value');
+      response['marklogic.com/r1']['marklogic.com/p1'][0].value.should.equal('literal1');
+      done();
+      })
+    .catch(done);
+  });
+  it('should run a bound SPARQL update', function(done) {
+    this.timeout(3000);
+    db.graphs.sparqlUpdate({
+        usingNamedGraphs: true,
+        data:
+          'CREATE GRAPH <'+graph3Uri+'> ; '+
+          'INSERT { GRAPH <'+graph3Uri+'> { ?s ?p "literal2" } } '+
+          'WHERE  { GRAPH <'+graph2Uri+'> {?s ?p ?o. filter (?p = ?b1) } }',
+        bindings: {
+          b1: 'marklogic.com/p1',
+          b2: {value:'a value', type:'string'}
+          }
+      })
+    .result(function(response) {
+      return db.graphs.read(graph3Uri, 'application/rdf+json').result();
+      })
+    .then(function(response) {
+      response.should.have.property('marklogic.com/r1');
+      response['marklogic.com/r1'].should.have.property('marklogic.com/p1');
+      response['marklogic.com/r1']['marklogic.com/p1'].length.should.equal(1);
+      response['marklogic.com/r1']['marklogic.com/p1'][0].should.have.property('value');
+      response['marklogic.com/r1']['marklogic.com/p1'][0].value.should.equal('literal2');
+      done();
+      })
+    .catch(done);
+  });
+  it('should delete the SPARQL update graph', function(done){
+    db.graphs.remove(graph2Uri)
+    .result(function(response){
+      return db.graphs.remove(graph3Uri).result();
+      })
+    .then(function(response) {
       done();
       })
     .catch(done);
