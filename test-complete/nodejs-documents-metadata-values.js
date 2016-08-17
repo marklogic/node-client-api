0d796ae996bccfccc3f4bb4cd9a98cb00e326d8b
Aries Yuwono <ayuwono@ayuwono-z620.marklogic.com>
2016-08-17 14:03:32 -0700

    added

---
+++ test-complete/nodejs-documents-metadata-values.js
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2014-2016 MarkLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var should = require('should');
+
+var testconfig = require('../etc/test-config-qa.js');
+
+var marklogic = require('../');
+var p = marklogic.patchBuilder;
+
+var db = marklogic.createDatabaseClient(testconfig.restWriterConnection);
+var dbReader = marklogic.createDatabaseClient(testconfig.restReaderConnection);
+var dbAdmin = marklogic.createDatabaseClient(testconfig.restAdminConnection);
+
+describe('Document Metadata values test', function() {
+
+  it('TEST 1 - write document content and metadata', function(done) {
+    db.documents.write({
+      uri: '/test/metadata/values/doc1.json',
+      collections: ['metadataValuesColl'],
+      contentType: 'application/json',
+      quality: 250,
+      properties: {
+        prop1:'bar', 
+        prop2:1981
+      },
+      metadataValues: {
+        meta1: 'super plastic',
+        meta2: 45.89,
+        meta3: true,
+        meta4: null,
+        meta5: undefined,
+        metaDateTime: '2011-12-31T23:59:59'
+      },
+      content: {
+        id: '0001', 
+        name:'bowl'
+      }
+    }).
+    result(function(response) {
+      //console.log(JSON.stringify(response, null, 2));
+      response.documents[0].uri.should.equal('/test/metadata/values/doc1.json');     
+      done();
+    }, done);
+  });
+
+  it('TEST 2 - read the content and metadata values', function(done) {
+    db.documents.read({
+      uris: ['/test/metadata/values/doc1.json'], 
+      categories:['content', 'metadataValues']
+    }).result(function(documents) {
+      //console.log(JSON.stringify(documents, null, 4));
+      documents[0].metadataValues.meta1.should.equal('super plastic');
+      documents[0].metadataValues.meta2.should.equal(45.89);
+      documents[0].metadataValues.meta3.should.equal(true);
+      documents[0].metadataValues.meta4.should.be.empty;
+      documents[0].metadataValues.metaDateTime.should.equal('2011-12-31T23:59:59');
+      done();
+    }, done);
+  });
+
+  it('TEST 3 - insert patch on metadata values', function(done) {
+    db.documents.patch({
+      uri: '/test/metadata/values/doc1.json', 
+      categories:['metadata-values'],
+      operations: [
+        p.insert('metadataValues', 'last-child', {meta6: 'added patch'})
+      ]
+    }).result(function(documents) {
+      //console.log(JSON.stringify(documents, null, 4));
+      documents.uri.should.equal('/test/metadata/values/doc1.json');
+      done();
+    }, done);
+  });
+
+  it('TEST 4 - read the patched metadata values', function(done) {
+    db.documents.read({
+      uris: ['/test/metadata/values/doc1.json'], 
+      categories:['metadataValues']
+    }).result(function(documents) {
+      //console.log(JSON.stringify(documents, null, 4));
+      documents[0].metadataValues.meta6.should.equal('added patch');
+      done();
+    }, done);
+  });
+
+  it('TEST 5 - remove patch on metadata values', function(done) {
+    db.documents.patch({
+      uri: '/test/metadata/values/doc1.json', 
+      categories:['metadata-values'],
+      operations: [
+        p.remove('meta6')
+      ]
+    }).result(function(documents) {
+      //console.log(JSON.stringify(documents, null, 4));
+      documents.uri.should.equal('/test/metadata/values/doc1.json');
+      done();
+    }, function(error) {console.log(error); done();});
+  });
+
+  it('TEST 6 - read the patched metadata values', function(done) {
+    db.documents.read({
+      uris: ['/test/metadata/values/doc1.json'], 
+      categories:['metadataValues']
+    }).result(function(documents) {
+      //console.log(JSON.stringify(documents, null, 4));
+      documents[0].metadataValues.should.not.have.property('meta6');
+      done();
+    }, done);
+  });
+
+  it('TEST 7 - insert patch on existing metadata values', function(done) {
+    db.documents.patch({
+      uri: '/test/metadata/values/doc1.json', 
+      categories:['metadata-values'],
+      operations: [
+        p.insert('metadataValues', 'last-child', {meta1: 'this is the date'})
+      ]
+    }).result(function(documents) {
+      //console.log(JSON.stringify(documents, null, 4));
+      documents.uri.should.equal('/test/metadata/values/doc1.json');
+      done();
+    }, done);
+  });
+
+  it('TEST 8 - read the patched metadata values', function(done) {
+    db.documents.read({
+      uris: ['/test/metadata/values/doc1.json'], 
+      categories:['metadataValues']
+    }).result(function(documents) {
+      //console.log(JSON.stringify(documents, null, 4));
+      documents[0].metadataValues.meta1.should.equal('this is the date');
+      done();
+    }, done);
+  });
+
+  /*it('should delete all documents', function(done){
+    dbAdmin.documents.removeAll({
+      collection: 'metadataValuesColl'
+    }).
+    result(function(response) {
+      done();
+    }, done);
+  });*/
+
+});
