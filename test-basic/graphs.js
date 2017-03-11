e786d3351bec5fbfa9691131e719da4c6b1a8527
Mike Wooldridge <mike@hellion.org>
2017-03-10 16:43:12 -0800

    fix(#383): Change usingDefaultGraphs and usingNamedGraphs param types
    Params each take one or more graph URIs, not a boolean

--- test-basic/graphs.js
+++ test-basic/graphs.js
@@ -229,7 +229,8 @@ describe('graph operations', function(){
   it('should run a bound SPARQL update', function(done) {
     this.timeout(3000);
     db.graphs.sparqlUpdate({
-        usingNamedGraphs: true,
+        usingDefaultGraphs: graph3Uri, // for coverage, superfluous given the query
+        usingNamedGraphs: [graph2Uri, graph3Uri], // for coverage, superfluous given the query
         data:
           'CREATE GRAPH <'+graph3Uri+'> ; '+
           'INSERT { GRAPH <'+graph3Uri+'> { ?s ?p "literal2" } } '+
