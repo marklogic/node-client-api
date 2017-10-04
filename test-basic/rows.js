6a2c63fbe70001e6e68168d600f0ebfe2f581444
Mike Wooldridge <mike@hellion.org>
2017-10-04 09:49:28 -0700

    Add test of planBuilder-built plan

--- test-basic/rows.js
+++ test-basic/rows.js
@@ -27,15 +27,47 @@ const planPathBindings = './test-basic/data/literalsBindings.json';
 const db = marklogic.createDatabaseClient(testconfig.restWriterConnection);
 //db.setLogger('debug');
 
+const p = marklogic.planBuilder;
+
 describe('rows', function(){
 
-  const plan = fs.readFileSync(planPath);
-  const planBindings = fs.readFileSync(planPathBindings);
+  const planFromJSON = fs.readFileSync(planPath, 'utf8');
+  const planFromJSONBindings = fs.readFileSync(planPathBindings, 'utf8');
+  const planFromBuilder = p.fromLiterals([
+            {"id": 1,"name": "Master 1","date": "2015-12-01"},
+            {"id": 2,"name": "Master 2","date": "2015-12-02"},
+            {"id": 3,"name": "Master 3","date": "2015-12-03"},
+          ])
+        .where(p.gt(p.col('id'), 1));
 
   describe('read as a promise', function(){
 
-    it('as an array of JSON objects', function(done){
-      db.rows.query(plan, {format: 'json'})
+    it('as an array of JSON objects using plan builder as an input', function(done){
+      db.rows.query(planFromBuilder, {format: 'json'})
+        .result(function(response) {
+          //console.log(JSON.stringify(response, null, 2));
+          valcheck.isArray(response).should.equal(true);
+          response.length.should.equal(3);
+          const obj1 = response[0];
+          valcheck.isObject(obj1).should.equal(true);
+          valcheck.isArray(obj1).should.equal(false);
+          obj1.should.have.property('columns');
+          valcheck.isArray(obj1['columns']).should.equal(true);
+          obj1['columns'][0].should.have.property('name');
+          obj1['columns'][0].should.not.have.property('type');
+          const obj2 = response[1];
+          valcheck.isObject(obj2).should.equal(true);
+          valcheck.isArray(obj2).should.equal(false);
+          obj2.should.have.property('date');
+          obj2['date'].should.have.property('type');
+          obj2['date'].should.have.property('value');
+          done();
+          })
+        .catch(done);
+    });
+
+    it('as an array of JSON objects using JSON as an input', function(done){
+      db.rows.query(planFromJSON, {format: 'json'})
         .result(function(response) {
           //console.log(JSON.stringify(response, null, 2));
           valcheck.isArray(response).should.equal(true);
@@ -59,7 +91,7 @@ describe('rows', function(){
     });
 
     it('as an array of JSON objects with header column types', function(done){
-      db.rows.query(plan, {format: 'json', columnTypes: 'header'})
+      db.rows.query(planFromJSON, {format: 'json', columnTypes: 'header'})
         .result(function(response) {
           //console.log(JSON.stringify(response, null, 2));
           valcheck.isArray(response).should.equal(true);
@@ -77,7 +109,7 @@ describe('rows', function(){
     });
 
     it('as an array of JSON objects with bindings', function(done){
-      db.rows.query(planBindings, {
+      db.rows.query(planFromJSONBindings, {
         format: 'json',
         bindings: {
           'limit': {value: 2, type: 'integer'},
@@ -94,7 +126,7 @@ describe('rows', function(){
     });
 
     it('as an array of XML elements', function(done){
-      db.rows.query(plan, {format: 'xml'})
+      db.rows.query(planFromJSON, {format: 'xml'})
         .result(function(response) {
           //console.log(JSON.stringify(response, null, 2));
           valcheck.isArray(response).should.equal(true);
@@ -119,7 +151,7 @@ describe('rows', function(){
       let chunks = 0,
           length = 0;
 
-      db.rows.query(plan, {format: 'json'})
+      db.rows.query(planFromJSON, {format: 'json'})
         .stream('chunked').
       on('data', function(chunk){
         // console.log(chunk);
@@ -142,7 +174,7 @@ describe('rows', function(){
     //   var chunks = 0,
     //       count = 0;
 
-    //   db.rows.query(plan, {format: 'json'})
+    //   db.rows.query(planFromJSON, {format: 'json'})
     //     .stream('object').
     //   on('data', function(obj){
     //     count++;
@@ -162,7 +194,7 @@ describe('rows', function(){
   describe('explain', function(){
 
     it('a plan as JSON', function(done){
-      db.rows.explain(plan, {format: 'json'})
+      db.rows.explain(planFromJSON, {format: 'json'})
         .result(function(response) {
           //console.log(JSON.stringify(response, null, 2));
           valcheck.isObject(response).should.equal(true);
@@ -175,7 +207,7 @@ describe('rows', function(){
     });
 
     it('a plan as XML', function(done){
-      db.rows.explain(plan, {format: 'xml'})
+      db.rows.explain(planFromJSON, {format: 'xml'})
         .result(function(response) {
           //console.log(JSON.stringify(response, null, 2));
           valcheck.isString(response).should.equal(true);
