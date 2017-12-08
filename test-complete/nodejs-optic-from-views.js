bb66adb3a47211146072cd7a8cf0173f91e87aff
Aries Yuwono <ayuwono@ayuwono-z620.marklogic.com>
2017-12-08 11:25:22 -0800

    added timestamp tests

--- test-complete/nodejs-optic-from-views.js
+++ test-complete/nodejs-optic-from-views.js
@@ -26,6 +26,7 @@ const db = marklogic.createDatabaseClient(connectdef.plan);
 const op = marklogic.planBuilder;
 
 describe('Nodejs Optic from views test', function(){
+  var oldTimestamp = db.createTimestamp('123');
 
   it('TEST 1 - join inner with keymatch - object structure, columnType rows', function(done){
     const plan1 =
@@ -1125,4 +1126,66 @@ describe('Nodejs Optic from views test', function(){
     }, done);
   });
 
+  it('TEST 37 - with timestamp', function(done){
+    const timestamp = db.createTimestamp();
+    const plan1 =
+      op.fromView('opticFunctionalTest', 'detail', 'myDetail');
+    const plan2 =
+      op.fromView('opticFunctionalTest', 'master', 'myMaster');
+    const masterIdCol1 = plan1.col({column: 'masterId'});
+    const masterIdCol2 = plan2.col('id');
+    const idCol1 = plan2.col('id');
+    const idCol2 = plan1.col({column: 'id'});
+    const detailNameCol = plan1.col({column: 'name'});
+    const masterNameCol = plan2.col('name');
+    const output =
+      plan1.joinInner({
+        right: plan2, 
+        keys: [op.on(masterIdCol1, masterIdCol2), op.on(idCol1, idCol2)]
+      })
+      .orderBy({keys: [op.desc({column: detailNameCol})]})
+      .offsetLimit({start: 1, length: 100})
+    db.rows.query(output, { format: 'json', structure: 'object', columnTypes: 'header', timestamp: timestamp }) 
+    .then(function(output) {
+      //console.log(JSON.stringify(output, null, 2));
+      expect(output.rows.length).to.equal(1);
+      expect(output.rows[0]['myMaster.id']).to.equal(1);
+      expect(output.rows[0]['myMaster.name']).to.equal('Master 1');
+      expect(output.rows[0]['myDetail.id']).to.equal(1);
+      expect(output.rows[0]['myDetail.name']).to.equal('Detail 1');
+      expect(output.rows[0]['myDetail.masterId']).to.equal(1);
+      done();
+    }, done);
+  });
+
+  it('TEST 38 - with old timestamp', function(done){
+    const timestamp = db.createTimestamp();
+    const plan1 =
+      op.fromView('opticFunctionalTest', 'detail', 'myDetail');
+    const plan2 =
+      op.fromView('opticFunctionalTest', 'master', 'myMaster');
+    const masterIdCol1 = plan1.col({column: 'masterId'});
+    const masterIdCol2 = plan2.col('id');
+    const idCol1 = plan2.col('id');
+    const idCol2 = plan1.col({column: 'id'});
+    const detailNameCol = plan1.col({column: 'name'});
+    const masterNameCol = plan2.col('name');
+    const output =
+      plan1.joinInner({
+        right: plan2, 
+        keys: [op.on(masterIdCol1, masterIdCol2), op.on(idCol1, idCol2)]
+      })
+      .orderBy({keys: [op.desc({column: detailNameCol})]})
+      .offsetLimit({start: 1, length: 100})
+    db.rows.query(output, { format: 'json', structure: 'object', columnTypes: 'header', timestamp: oldTimestamp }) 
+    .then(function(output) {
+      //console.log(JSON.stringify(output, null, 2));
+      expect(output).to.equal('SHOULD HAVE FAILED');
+      done();
+    }, function(error) { 
+      //console.log(error);
+      expect(error.body.errorResponse.message).to.contain('XDMP-OLDSTAMP: Timestamp too old'); 
+      done(); 
+    });
+  });
 });
