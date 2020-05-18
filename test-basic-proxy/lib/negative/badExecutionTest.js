788253a86d07ba84922e4c84fb5aebe0e1180674
ehennum <ehennum@marklogic.com>
2020-05-18 14:37:09 -0700

    client test for error mapping in the Data Service endpoint

--- test-basic-proxy/lib/negative/badExecutionTest.js
+++ test-basic-proxy/lib/negative/badExecutionTest.js
@@ -23,19 +23,48 @@ const BadExecution = require("./badExecution.js");
 describe('bad execution', function() {
   const service = BadExecution.on(testutil.makeClient());
 
-  it('error mapping', function (done) {
-    service.errorMapping()
+  const test = [
+    [406, 'Unacceptable1', "Unacceptable in every way"],
+    [406, 'Unacceptable2', "Unacceptable, that's how you'll stay", "Unacceptable, that's how you'll stay"],
+    [406, 'Unacceptable2', "Unacceptable, that's how you'll stay", null],
+    [406, 'Unacceptable2', "Unacceptable, that's how you'll stay", "Unacceptable no matter how near or far"],
+    [410, 'Absent1',       "You may search this wide world over"],
+    [500, 'Unknown1',      null,                                   "Are the stars out tonight?"]
+  ];
+
+  function runTest(i, done) {
+    if (i === test.length) {
+      done();
+    }
+    const testCase  = test[i];
+
+    const statusCode = testCase[0];
+    const sentCode   = testCase[1];
+    const mappedMsg  = (testCase[2] === null) ? 'Internal Server Error' : testCase[2];
+    const sentMsg    = (testCase.length < 4)  ? null                    : testCase[3];
+    const mappedCode = (testCase[2] === null) ? 'INTERNAL ERROR'        : sentCode;
+
+    service.errorMapping(sentCode, sentMsg)
         .then(output => {
           expect.fail('should not succeed');
           done();
         })
         .catch(err => {
           const errorResponse = err.body.errorResponse;
-          expect(errorResponse.statusCode).to.eql(418);
-          expect(errorResponse.status).to.eql("Status Message For 418");
-          expect(errorResponse.messageCode).to.eql('ERROR_MAPPING');
-          expect(errorResponse.message).to.contain('Test of error mapping');
-          done();
+          // console.log(errorResponse);
+          expect(errorResponse.statusCode).to.eql(statusCode);
+          expect(errorResponse.status).to.eql(mappedMsg);
+          expect(errorResponse.messageCode).to.eql(mappedCode);
+          if (sentMsg !== null) {
+            const inlineMsg = ': fn.error(null, errCode, errMsg); --  ';
+            const msgOffset = errorResponse.message.indexOf(inlineMsg) + inlineMsg.length;
+            expect(errorResponse.message.substring(msgOffset, msgOffset + sentMsg.length)).to.eql(sentMsg);
+          }
+          runTest(i + 1, done);
         });
+  }
+
+  it('error conditions', function (done) {
+    runTest(0, done);
   });
 });
