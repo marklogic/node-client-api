e4e28c3e41cc0ad3372e5527f9136c106dcfdef7
anu3990 <anushreegithub@gmail.com>
2021-05-25 15:42:40 -0700

    Endpoint Caller unit tests.

--- test-basic/endpoint-caller.js
+++ test-basic/endpoint-caller.js
@@ -20,16 +20,16 @@ var testconfig = require('../etc/test-config.js');
 var expect = require('chai').expect;
 var dbWriter = marklogic.createDatabaseClient(testconfig.restWriterConnection);
 
-describe('Service caller', function() {
+describe('Endpoint caller', function() {
 
     it('moduleInitSjs endpoint', function(done) {
         const serviceDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/moduleInitSjs/service.json', {encoding: 'utf8'}));
-        serviceDeclaration.endpointExtension = '.sjs';
         const endpointDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/moduleInitSjs/initializer.api', {encoding: 'utf8'}));
-        const serviceCaller = dbWriter.serviceCaller(serviceDeclaration, [endpointDeclaration]);
+        endpointDeclaration.endpoint = serviceDeclaration.endpointDirectory+'initializer'+'.sjs';
+        const endpointCaller = dbWriter.endpointCaller(endpointDeclaration);
         const params = {param1:true, param2: '1.2', param3: [1.2, 3.4], param4:[5, 6] };
 
-        serviceCaller.call('initializer', params)
+        endpointCaller.call(params)
             .then(output => {
                 expect(output).to.eql(true);
                 done();
@@ -42,12 +42,12 @@ describe('Service caller', function() {
 
     it('moduleInitXqy endpoint', function(done) {
         const serviceDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/moduleInitXqy/service.json', {encoding: 'utf8'}));
-        serviceDeclaration.endpointExtension = '.xqy';
         const endpointDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/moduleInitXqy/initializer.api', {encoding: 'utf8'}));
-        const serviceCaller = dbWriter.serviceCaller(serviceDeclaration, [endpointDeclaration]);
+        endpointDeclaration.endpoint = serviceDeclaration.endpointDirectory+'initializer'+'.xqy';
+        const endpointCaller = dbWriter.endpointCaller(endpointDeclaration);
         const params = {param1:true, param2: '1.2', param3: [1.2, 3.4], param4:[5, 6] };
 
-        serviceCaller.call('initializer', params)
+        endpointCaller.call(params)
             .then(output => {
                 expect(output).to.eql(true);
                 done();
@@ -61,13 +61,13 @@ describe('Service caller', function() {
     it('postOfMultipartForDocumentArray1 endpoint', function(done) {
         const serviceDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfMultipartForDocument/service.json',
             {encoding: 'utf8'}));
-        serviceDeclaration.endpointExtension = '.sjs';
         const endpointDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfMultipartForDocument/postOfMultipartForDocumentArray1.api',
             {encoding: 'utf8'}));
-        const serviceCaller = dbWriter.serviceCaller(serviceDeclaration, [endpointDeclaration]);
+        endpointDeclaration.endpoint = serviceDeclaration.endpointDirectory+'postOfMultipartForDocumentArray1'+'.sjs';
+        const endpointCaller = dbWriter.endpointCaller(endpointDeclaration);
         const params = {param1:[{"root":{"child":"text1"}}, {"root":{"child":"text2"}}]};
 
-        serviceCaller.call('postOfMultipartForDocumentArray1', params)
+        endpointCaller.call(params)
             .then(output => {
                 expect(output).to.eql(["text1", 1]);
                 done();
@@ -81,42 +81,15 @@ describe('Service caller', function() {
     it('postOfUrlencodedForDocumentArray1 endpoint', function(done) {
         const serviceDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfUrlencodedForDocument/service.json',
             {encoding: 'utf8'}));
-        serviceDeclaration.endpointExtension = '.mjs';
         const endpointDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfUrlencodedForDocument/postOfUrlencodedForDocumentArray1.api',
             {encoding: 'utf8'}));
-        const serviceCaller = dbWriter.serviceCaller(serviceDeclaration, [endpointDeclaration]);
+        endpointDeclaration.endpoint = serviceDeclaration.endpointDirectory+'postOfUrlencodedForDocumentArray1'+'.mjs';
+        const endpointCaller = dbWriter.endpointCaller(endpointDeclaration);
         const params = {param1:[1.2, 3.4]};
 
-        serviceCaller.call('postOfUrlencodedForDocumentArray1', params)
+        endpointCaller.call(params)
             .then(output => {
                 expect(output).to.eql(["text1", 1]);
-                done();
-            })
-            .catch(err => {
-                expect.fail(err.toString());
-                done();
-            });
-    });
-
-    it('verify support for multiple endpoint declarations', function(done) {
-        const serviceDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfMultipartForDocument/service.json',
-            {encoding: 'utf8'}));
-        serviceDeclaration.endpointExtension = '.sjs';
-        const endpointDeclaration1 = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfMultipartForDocument/postOfMultipartForDocumentArray1.api',
-            {encoding: 'utf8'}));
-        const endpointDeclaration2 = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfMultipartForDocument/postOfMultipartForDocumentTextDocument1.api',
-            {encoding: 'utf8'}));
-        const serviceCaller = dbWriter.serviceCaller(serviceDeclaration, [endpointDeclaration1, endpointDeclaration2]);
-        const params1 = {param1:[1.2, 3.4]};
-        const params2 = {param1:[{"root":{"child":"text1"}}, {"root":{"child":"text2"}}]};
-
-        serviceCaller.call('postOfMultipartForDocumentArray1', params1)
-            .then(output => {
-                expect(output).to.eql(["text1", 1]);
-                return serviceCaller.call('postOfMultipartForDocumentTextDocument1', params2);
-            })
-            .then(output => {
-                expect(output).to.eql('abc');
                 done();
             })
             .catch(err => {
