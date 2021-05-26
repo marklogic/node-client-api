3272f4dad50d5501fe8368704397c89fe5188186
anu3990 <anushreegithub@gmail.com>
2021-05-26 15:31:40 -0700

    Fixed the module path in - endpoint-caller.js

--- test-basic/endpoint-caller.js
+++ test-basic/endpoint-caller.js
@@ -23,8 +23,8 @@ var dbWriter = marklogic.createDatabaseClient(testconfig.restWriterConnection);
 describe('Endpoint caller', function() {
 
     it('moduleInitSjs endpoint', function(done) {
-        const serviceDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/moduleInitSjs/service.json', {encoding: 'utf8'}));
-        const endpointDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/moduleInitSjs/initializer.api', {encoding: 'utf8'}));
+        const serviceDeclaration = JSON.parse(fs.readFileSync('./test-basic-proxy/ml-modules/generated/moduleInitSjs/service.json', {encoding: 'utf8'}));
+        const endpointDeclaration = JSON.parse(fs.readFileSync('./test-basic-proxy/ml-modules/generated/moduleInitSjs/initializer.api', {encoding: 'utf8'}));
         endpointDeclaration.endpoint = serviceDeclaration.endpointDirectory+endpointDeclaration.functionName+'.sjs';
         const endpointCaller = dbWriter.endpointCaller(endpointDeclaration);
         const params = {param1:true, param2: '1.2', param3: [1.2, 3.4], param4:[5, 6] };
@@ -41,8 +41,8 @@ describe('Endpoint caller', function() {
     });
 
     it('moduleInitXqy endpoint', function(done) {
-        const serviceDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/moduleInitXqy/service.json', {encoding: 'utf8'}));
-        const endpointDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/moduleInitXqy/initializer.api', {encoding: 'utf8'}));
+        const serviceDeclaration = JSON.parse(fs.readFileSync('./test-basic-proxy/ml-modules/generated/moduleInitXqy/service.json', {encoding: 'utf8'}));
+        const endpointDeclaration = JSON.parse(fs.readFileSync('./test-basic-proxy/ml-modules/generated/moduleInitXqy/initializer.api', {encoding: 'utf8'}));
         endpointDeclaration.endpoint = serviceDeclaration.endpointDirectory+endpointDeclaration.functionName+'.xqy';
         const endpointCaller = dbWriter.endpointCaller(endpointDeclaration);
         const params = {param1:true, param2: '1.2', param3: [1.2, 3.4], param4:[5, 6] };
@@ -59,9 +59,9 @@ describe('Endpoint caller', function() {
     });
 
     it('postOfMultipartForDocumentArray1 endpoint', function(done) {
-        const serviceDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfMultipartForDocument/service.json',
+        const serviceDeclaration = JSON.parse(fs.readFileSync('./test-basic-proxy/ml-modules/generated/postOfMultipartForDocument/service.json',
             {encoding: 'utf8'}));
-        const endpointDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfMultipartForDocument/postOfMultipartForDocumentArray1.api',
+        const endpointDeclaration = JSON.parse(fs.readFileSync('./test-basic-proxy/ml-modules/generated/postOfMultipartForDocument/postOfMultipartForDocumentArray1.api',
             {encoding: 'utf8'}));
         endpointDeclaration.endpoint = serviceDeclaration.endpointDirectory+endpointDeclaration.functionName+'.sjs';
         const endpointCaller = dbWriter.endpointCaller(endpointDeclaration);
@@ -79,9 +79,9 @@ describe('Endpoint caller', function() {
     });
 
     it('postOfUrlencodedForDocumentArray1 endpoint', function(done) {
-        const serviceDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfUrlencodedForDocument/service.json',
+        const serviceDeclaration = JSON.parse(fs.readFileSync('./test-basic-proxy/ml-modules/generated/postOfUrlencodedForDocument/service.json',
             {encoding: 'utf8'}));
-        const endpointDeclaration = JSON.parse(fs.readFileSync('test-basic-proxy/ml-modules/generated/postOfUrlencodedForDocument/postOfUrlencodedForDocumentArray1.api',
+        const endpointDeclaration = JSON.parse(fs.readFileSync('./test-basic-proxy/ml-modules/generated/postOfUrlencodedForDocument/postOfUrlencodedForDocumentArray1.api',
             {encoding: 'utf8'}));
         endpointDeclaration.endpoint = serviceDeclaration.endpointDirectory+endpointDeclaration.functionName+'.mjs';
         const endpointCaller = dbWriter.endpointCaller(endpointDeclaration);
