a5b4b59a85ce5b26af7719652d059344245d4c62
anu3990 <anushreegithub@gmail.com>
2021-12-21 12:36:57 -0800

    readAll api - Implementation for chunked outputStreamType and unit tests for transform, categories, consistentSnapshot and onBatchError.

--- test-basic/documents-data-movement-readAll.js
+++ test-basic/documents-data-movement-readAll.js
@@ -18,12 +18,19 @@ const should = require('should');
 const testconfig = require('../etc/test-config.js');
 const marklogic = require('../');
 const dbWriter = marklogic.createDatabaseClient(testconfig.restWriterConnection);
+const restAdminDB = marklogic.createDatabaseClient(testconfig.restAdminConnection);
 const Stream = require('stream');
 const streamToArray = require('stream-to-array');
+const fs = require('fs');
+const expect = require('chai').expect;
+
 let uriStream = new Stream.Readable();
 let urisList = [];
 let result = new Set();
 let summaryValue = null;
+let categoriesUrisList = [];
+let xqyTransformName = 'flagParam';
+let xqyTransformPath = './test-basic/data/flagTransform.xqy';
 
 describe('data movement readAll', function() {
     before(function (done) {
@@ -38,23 +45,58 @@ describe('data movement readAll', function() {
             urisList.push(temp.uri);
         }
         readable.push(null);
-        readable.pipe(dbWriter.documents.writeAll());
-        setTimeout(()=>{done();}, 3000);
+        readable.pipe(dbWriter.documents.writeAll({
+            onCompletion: ((summary) => {
+                readable = new Stream.Readable({objectMode: true});
+
+                for (let i = 0; i < 400; i++) {
+                    const temp = {
+                        uri: '/test/dataMovement/requests/readAll/categories/' + i + '.json',
+                        contentType: 'application/json',
+                        content: {['key ' + i]: 'value ' + i}
+                    };
+                    readable.push(temp);
+                    categoriesUrisList.push(temp.uri);
+                }
+                readable.push(null);
+
+                readable.pipe(dbWriter.documents.writeAll({
+                    defaultMetadata: {
+                        metadataValues: {
+                            'metadataKey1': 'metadataValue 1',
+                            'metadataKey2': 'metadataValue 2'
+                        },
+                        collections: 'collection1',
+                        permissions: [
+                            {'role-name': 'app-user', capabilities: ['read']},
+                            {'role-name': 'app-builder', capabilities: ['read', 'update']}
+                        ],
+                        quality: 1
+                    },
+                    onCompletion: ((summary) => {
+                        done();
+                    })
+                }));
+            })
+        }));
     });
 
     beforeEach(function(done){
         uriStream = new Stream.PassThrough({objectMode: true});
         urisList.forEach(uri => uriStream.push(uri));
-        setTimeout(()=>{uriStream.push(null);done();}, 3000);
+        uriStream.push(null);
+        done();
     });
 
     after((function(done){
+        categoriesUrisList.forEach(uri=>urisList.push(uri));
         dbWriter.documents.remove(urisList)
-            .result(function(){
-                done();
-            })
-            .catch(err=> done(err))
-            .catch(done);
+        .result(function(){
+            restAdminDB.config.transforms.remove(xqyTransformName);
+            done();
+        })
+        .catch(err=> done(err))
+        .catch(done);
     }));
 
     it('should readAll documents with empty options', function(done){
@@ -64,8 +106,7 @@ describe('data movement readAll', function() {
                 if(err){
                     done(err);
                 }
-                arr.forEach(item=> result.add(item.uri)
-                );
+                arr.forEach(item=> result.add(item.uri));
                 checkResult(done);
             });
     });
@@ -101,8 +142,7 @@ describe('data movement readAll', function() {
                 if(err){
                     done(err);
                 }
-                arr.forEach(item=> result.add(item.uri)
-                );
+                arr.forEach(item=> result.add(item.uri));
                 checkResult(done);
             });
     });
@@ -121,8 +161,7 @@ describe('data movement readAll', function() {
                 if(err){
                     done(err);
                 }
-                arr.forEach(item=> result.add(item.uri)
-                );
+                arr.forEach(item=> result.add(item.uri));
                 checkResult(done);
             });
     });
@@ -136,6 +175,18 @@ describe('data movement readAll', function() {
         }
     });
 
+    it('should throw error with categories and outputStreamType as chunked', function(done){
+        try{
+            dbWriter.documents.readAll({
+                outputStreamType: 'chunked',
+                categories: []
+            });
+        } catch(err){
+            err.toString().should.equal('Error: categories not expected when outputStreamType is chunked.');
+            done();
+        }
+    });
+
     it('should readAll documents with outputStreamType option as object', function(done){
         streamToArray(uriStream.pipe(dbWriter.documents.readAll({
                 outputStreamType: 'Object'
@@ -144,8 +195,7 @@ describe('data movement readAll', function() {
                 if(err){
                     done(err);
                 }
-                arr.forEach(item=> result.add(item.uri)
-                );
+                arr.forEach(item=> result.add(item.uri));
                 checkResult(done);
             });
     });
@@ -158,13 +208,12 @@ describe('data movement readAll', function() {
                 if(err){
                     done(err);
                 }
-                arr.forEach(item=> result.add(JSON.parse(item).uri));
-                checkResult(done);
+                arr.forEach(item=> result.add(item.toString()));
+
+                result.size.should.equal(10000);
+                result.clear();
+                done();
             });
-       /* uriStream.pipe(dbWriter.documents.readAll({
-            outputStreamType: 'chunked'
-        })).pipe(fs.createWriteStream('file.txt'));
-       */
     });
 
     it('should throw error with invalid batchSize option', function(done){
@@ -184,8 +233,7 @@ describe('data movement readAll', function() {
                 if(err){
                     done(err);
                 }
-                arr.forEach(item=> result.add(item.uri)
-                );
+                arr.forEach(item=> result.add(item.uri));
                 checkResult(done);
             });
     });
@@ -216,10 +264,143 @@ describe('data movement readAll', function() {
                 if(err){
                     done(err);
                 }
-                arr.forEach(item=> result.add(item.uri)
-                );
+                arr.forEach(item=> result.add(item.uri));
                 checkResult(done);
             });
+    });
+
+    it('should readAll documents with categories option', function(done){
+        uriStream = new Stream.PassThrough({objectMode: true});
+        categoriesUrisList.forEach(uri => uriStream.push(uri));
+        uriStream.push(null);
+
+         streamToArray(uriStream.pipe(dbWriter.documents.readAll({
+                categories: ['permissions', 'metadataValues', 'collections', 'quality']
+            })),
+            function (err, arr) {
+                if (err) {
+                    done(err);
+                }
+                arr.forEach(item => result.add(item));
+                checkCategoriesResult(result, done);
+            });
+    });
+
+    it('should readAll documents with transform option', function(done){
+        this.timeout(15000);
+        restAdminDB.config.transforms.write(xqyTransformName, 'xquery', fs.createReadStream(xqyTransformPath))
+            .result(function(response){
+
+                streamToArray(uriStream.pipe(dbWriter.documents.readAll({
+                        transform: [xqyTransformName, {flag:'tested1'}]
+                    })),
+                    function(err, arr ) {
+                        if(err){
+                            done(err);
+                        }
+                        arr.forEach(item=> result.add(item));
+                        checkTransformResult(done);
+                    });
+            })
+            .catch(err=> done(err));
+    });
+
+    it('should throw error with invalid onBatchError option', function(done){
+        restAdminDB.config.transforms.write(xqyTransformName, 'xquery', fs.createReadStream(xqyTransformPath))
+            .result(function(response){
+                uriStream.pipe(dbWriter.documents.readAll({
+                    batchSize:1000,
+                    transform: ['tested1'],
+                    onBatchError: ((progressSoFar, documents, error) => {
+                        progressSoFar.docsReadSuccessfully.should.be.equal(0);
+                        progressSoFar.docsFailedToBeRead.should.be.equal(1000);
+                        error.body.errorResponse.messageCode.should.be.equal('RESTAPI-INVALIDREQ');
+                        documents.length.should.equal(1000);
+                        return 10;
+                    })
+                })).on('error', function(err){
+                    err.toString().should.equal('Error: onBatchError should return null, empty array or a replacement array.');
+                    done();
+                });
+            })
+            .catch(err=> done(err));
+    });
+
+    it('should readAll documents with onBatchError option returning null', function(done){
+        restAdminDB.config.transforms.write(xqyTransformName, 'xquery', fs.createReadStream(xqyTransformPath))
+            .result(function(response){
+                uriStream.pipe(dbWriter.documents.readAll({
+                    batchSize:1000,
+                    transform: ['tested1'],
+                    onBatchError: ((progressSoFar, documents, error) => {
+                        progressSoFar.docsReadSuccessfully.should.be.equal(0);
+                        progressSoFar.docsFailedToBeRead.should.be.equal(1000);
+                        error.body.errorResponse.messageCode.should.be.equal('RESTAPI-INVALIDREQ');
+                        documents.length.should.equal(1000);
+                        return null;
+                    }),
+                    onCompletion: ((summary) => {
+                        summary.docsReadSuccessfully.should.be.equal(0);
+                        summary.docsFailedToBeRead.should.be.equal(10000);
+                        summary.timeElapsed.should.be.greaterThanOrEqual(0);
+                        done();
+                    })
+                }));
+            })
+            .catch(err=> done(err));
+    });
+
+    it('should readAll documents with onBatchError option returning replacement batch', function(done){
+        let onBatchErrorStream = new Stream.PassThrough({objectMode: true});
+        onBatchErrorStream.push('');
+        onBatchErrorStream.push(null);
+        onBatchErrorStream.pipe(dbWriter.documents.readAll({
+            batchSize:10,
+            onBatchError: ((progressSoFar, documents, error) => {
+                let replacementBatch = [];
+                for (let i = 0; i < 10; i++) {
+                    replacementBatch.push('/test/dataMovement/requests/readAll/' + i + '.json');
+                }
+
+                onBatchErrorStream = new Stream.PassThrough({objectMode: true});
+                onBatchErrorStream.push(null);
+                return replacementBatch;
+            }),
+            onCompletion: ((summary) => {
+                summary.docsReadSuccessfully.should.be.equal(10);
+                summary.docsFailedToBeRead.should.be.equal(0);
+                summary.timeElapsed.should.be.greaterThanOrEqual(0);
+                done();
+            })
+        }));
+    });
+
+    it('should readAll documents with consistentSnapshot option as true', function(done){
+        this.timeout(15000);
+        uriStream.pipe(dbWriter.documents.readAll({
+            consistentSnapshot:true,
+            onCompletion: ((summary) => {
+                summary.docsReadSuccessfully.should.be.equal(10000);
+                summary.docsFailedToBeRead.should.be.equal(0);
+                summary.timeElapsed.should.be.greaterThanOrEqual(0);
+                summary.consistentSnapshotTimestamp.should.be.greaterThanOrEqual(0);
+                done();
+            })
+        }));
+    });
+
+    it('should readAll documents with consistentSnapshot option as false', function(done){
+
+        uriStream.pipe(dbWriter.documents.readAll({
+            consistentSnapshot:false,
+            onCompletion: ((summary) => {
+                summary.docsReadSuccessfully.should.be.equal(10000);
+                summary.docsFailedToBeRead.should.be.equal(0);
+                summary.timeElapsed.should.be.greaterThanOrEqual(0);
+                expect(summary.consistentSnapshotTimestamp).to.be.undefined;
+                done();
+            })
+        }));
     });
 });
 
@@ -228,7 +409,8 @@ function checkResult(done){
     for(let i=0; i<urisList.length; i++){
         result.has(urisList[i]).should.equal(true);
     }
-    setTimeout(()=>{result.clear();done();}, 4000);
+    result.clear();
+    done();
 }
 
 function checkSummary(summary, done) {
@@ -236,4 +418,38 @@ function checkSummary(summary, done) {
     summary.docsFailedToBeRead.should.be.equal(0);
     summary.timeElapsed.should.be.greaterThanOrEqual(0);
     checkResult(done);
+}
+
+function checkCategoriesResult(result, done){
+    result.size.should.equal(400);
+    result.forEach(item => {
+        item.collections[0].should.be.equal('collection1');
+        item.quality.should.be.equal(1);
+        item.metadataValues.metadataKey1.should.be.equal('metadataValue 1');
+        item.metadataValues.metadataKey2.should.be.equal('metadataValue 2');
+        item.permissions.forEach(function (permission) {
+            switch (permission['role-name']) {
+                case 'app-user':
+                    permission.capabilities.length.should.equal(1);
+                    permission.capabilities[0].should.equal('read');
+                    break;
+                case 'app-builder':
+                    permission.capabilities.length.should.equal(2);
+                    permission.capabilities.should.containEql('read');
+                    permission.capabilities.should.containEql('update');
+                    break;
+            }
+        });
+    });
+    result.clear();
+    done();
+}
+
+function checkTransformResult(done){
+    result.size.should.be.equal(10000);
+    result.forEach(item => {
+        item.content.flagParam.should.be.equal('tested1');
+    });
+    result.clear();
+    done();
 }
