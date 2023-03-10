8cde53cdebe828b871dcc6dc5fa58c4cebec7bba
anu3990 <anushreegithub@gmail.com>
2023-03-09 16:13:24 -0800

    NODE-125 : Create a new user with roles

--- etc/test-setup-users.js
+++ etc/test-setup-users.js
@@ -166,6 +166,131 @@ function setupUsers(manager, done) {
   }).
   then(function(response) {
     return manager.get({
+      endpoint: '/manage/v2/roles/'+encodeURIComponent('eval')
+    }).result();
+  }).
+  then(function(response) {
+    if (response.statusCode < 400) {
+      return this;
+    }
+    return manager.post({
+      endpoint: '/manage/v2/roles',
+      body: {
+        'role-name': 'eval',
+        description: 'eval role',
+        role: [],
+        privilege: [
+            {
+          'privilege-name': 'xdbc-eval',
+          action: 'http://marklogic.com/xdmp/privileges/xdbc-eval',
+          kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-eval-in',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-eval-in',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-invoke',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-invoke',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-invoke-in',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-invoke-in',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-spawn',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-spawn',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-spawn-in',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-spawn-in',
+            kind: 'execute'
+          }
+        ]
+      }
+    }).result();
+  }).
+  then(function(response) {
+    return manager.get({
+      endpoint: '/manage/v2/roles/'+encodeURIComponent('qbvuser-role')
+    }).result();
+  }).
+  then(function(response) {
+    if (response.statusCode < 400) {
+      return this;
+    }
+    return manager.post({
+      endpoint: '/manage/v2/roles',
+      body: {
+        'role-name': 'qbvuser-role',
+        description: 'qbvuser role for test-complete tests',
+        role: ['tde-view', 'query-view-admin','app-user','eval','harmonized-reader','rest-extension-user','rest-reader','rest-writer','rest-admin','tde-admin','manage-user','sparql-update-user'],
+        privilege: [
+            {
+            'privilege-name': 'any-uri',
+            action: 'http://marklogic.com/xdmp/privileges/any-uri',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-eval',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-eval',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-eval-in',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-eval-in',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'sem-sparql',
+            action: 'http://marklogic.com/xdmp/privileges/sem-sparql',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdbc-eval',
+            action: 'http://marklogic.com/xdmp/privileges/xdbc-eval',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdbc-invoke',
+            action: 'http://marklogic.com/xdmp/privileges/xdbc-invoke',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-sql',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-sql',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-http-get',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-http-get',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdmp-http-post',
+            action: 'http://marklogic.com/xdmp/privileges/xdmp-http-post',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdbc-invoke-in',
+            action: 'http://marklogic.com/xdmp/privileges/xdbc-invoke-in',
+            kind: 'execute'
+          },
+          {
+            'privilege-name': 'xdbc-eval-in',
+            action: 'http://marklogic.com/xdmp/privileges/xdbc-eval-in',
+            kind: 'execute'
+          }
+        ]
+      }
+    }).result();
+  }).
+  then(function(response) {
+    return manager.get({
       endpoint: '/manage/v2/users'
       }).result();
   }).
@@ -221,6 +346,13 @@ function setupUsers(manager, done) {
       'user-name': userName,
       description: 'test user to write tde to modules database',
       password:    testconfig.tdeConnection.password
+    };
+    userName = 'qbvuser';
+    requiredUsers[userName]  = {
+      role: 'qbvuser-role',
+      'user-name': userName,
+      description: 'qbvuser for test complete tests.',
+      password:    'qbvuser'
     };
 
     response.data['user-default-list']['list-items']['list-item'].
