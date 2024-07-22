4d8fe87b5c0fa62046f44ea19d17f8220d1a3678
asinha <ansinha@progress.com>
2024-07-22 11:55:38 -0700

    MLE-4113 : Submit Optic fromSearch plan with BM25 scoring with Node Client

--- test-basic/plan-search.js
+++ test-basic/plan-search.js
@@ -23,8 +23,20 @@ const p = marklogic.planBuilder;
 const pbb = require('./plan-builder-base');
 const execPlan = pbb.execPlan;
 const getResults = pbb.getResults;
+const assert = require('assert');
+const testlib = require("../etc/test-lib");
+let serverConfiguration = {};
 
 describe('search', function() {
+  before(function (done) {
+    this.timeout(6000);
+    try {
+      testlib.findServerConfiguration(serverConfiguration);
+      setTimeout(()=>done(), 3000);
+    } catch(error){
+      done(error);
+    }
+  });
   describe('accessors', function() {
     it('basic', function(done) {
       execPlan(
@@ -37,7 +49,7 @@ describe('search', function() {
         should(output[1].score.value).greaterThan(0);
         should(output[0].score.value).greaterThanOrEqual(output[1].score.value);
         done();
-      }).catch(done);
+      }).catch(error => done(error));
     });
     it('query shortcut', function(done) {
       execPlan(
@@ -50,7 +62,7 @@ describe('search', function() {
         should(output[1].score.value).greaterThan(0);
         should(output[0].score.value).greaterThanOrEqual(output[1].score.value);
         done();
-      }).catch(done);
+      }).catch(error => done(error));
     });
     it('columns', function(done) {
       execPlan(
@@ -66,7 +78,7 @@ describe('search', function() {
         should(output[1].fitness.value).greaterThanOrEqual(0);
         should(output[1].weight.value).greaterThanOrEqual(0);
         done();
-      }).catch(done);
+      }).catch(error => done(error));
     });
     it('qualifier', function(done) {
       execPlan(
@@ -79,7 +91,7 @@ describe('search', function() {
         should(output[1]['relevance.score'].value).greaterThan(0);
         should(output[0]['relevance.score'].value).greaterThanOrEqual(output[1]['relevance.score'].value);
         done();
-      }).catch(done);
+      }).catch(error => done(error));
     });
     it('qualified columns', function(done) {
       execPlan(
@@ -96,9 +108,9 @@ describe('search', function() {
         should(output[1]['relevance.fitness'].value).greaterThanOrEqual(0);
         should(output[1]['relevance.weight'].value).greaterThanOrEqual(0);
         done();
-      }).catch(done);
+      }).catch(error => done(error));
     });
-    it('columns', function(done) {
+    it('columns with options', function(done) {
       execPlan(
           p.fromSearch(p.cts.jsonPropertyValueQuery('instrument', 'trumpet'),
                   ['score', 'quality'], null, {scoreMethod:'simple', qualityWeight:0})
@@ -110,7 +122,7 @@ describe('search', function() {
         should(output[1].score.value).greaterThan(0);
         should(output[1].quality.value).greaterThanOrEqual(0);
         done();
-      }).catch(done);
+      }).catch(error => done(error));
     });
     it('with fragment id column', function(done) {
       execPlan(
@@ -122,7 +134,7 @@ describe('search', function() {
         should(output.length).equal(1);
         should(output[0].fragmentIdCheck.value).equal(true);
         done();
-      }).catch(done);
+      }).catch(error => done(error));
     });
   });
   describe('convenience', function() {
@@ -145,7 +157,7 @@ describe('search', function() {
         should(output[1].doc.value.musician.instrument[0]).equal('trumpet');
         should(output[0].score.value).greaterThanOrEqual(output[1].score.value);
         done();
-      }).catch(done);
+      }).catch(error => done(error));
     });
     it('qualified', function(done) {
       execPlan(
@@ -166,8 +178,79 @@ describe('search', function() {
         should(output[1]['relevance.doc'].value.musician.instrument[0]).equal('trumpet');
         should(output[0]['relevance.score'].value).greaterThanOrEqual(output[1]['relevance.score'].value);
         done();
-      }).catch(done);
+      }).catch(error => done(error));
     });
 // console.log(JSON.stringify(output, null, 2));
+  });
+
+  describe('tests for new scoring methods -  bm25, random and zero.', function() {
+    before(function (done) {
+      if(serverConfiguration.serverVersion < 12) {
+        this.skip();
+      }
+      done();
+    });
+
+    it('should search documents with bm25', function(done) {
+      execPlan(
+          p.fromSearch(p.cts.jsonPropertyValueQuery('instrument', 'trumpet'),
+              ['score', 'quality'], null, { scoreMethod: 'bm25', bm25LengthWeight: 0.25 })
+      ).then(function(response) {
+        assert(response.columns != null)
+        assert(response.rows != null)
+        done();
+      }).catch(error => done(error));
+    });
+
+    it('should throw error with invalid bm25LengthWeight', function(done) {
+      execPlan(
+          p.fromSearch(p.cts.jsonPropertyValueQuery('instrument', 'trumpet'),
+              ['score', 'quality'], null, { scoreMethod: 'bm25', bm25LengthWeight: 99 })
+      ).catch(error => {
+        try{
+          assert(error.body.errorResponse.message.toString().includes('Invalid option "bm25-length-weight=99'));
+          done();
+        } catch(error){
+          done(error);
+        }
+      });
+    });
+
+    it('should throw error with string values for bm25LengthWeight', function(done) {
+      try {
+        execPlan(
+            p.fromSearch(p.cts.jsonPropertyValueQuery('instrument', 'trumpet'),
+                ['score', 'quality'], null, { scoreMethod: 'bm25', bm25LengthWeight: 'abc' })
+        );
+      } catch(error){
+        assert(error.message.toString().includes('bm25LengthWeight must be a number'));
+        done();
+      }
+    });
+
+    it('should search documents with zero scoring method', function(done) {
+      execPlan(
+          p.fromSearch(p.cts.jsonPropertyValueQuery('instrument', 'trumpet'),
+              ['score', 'quality'], null, { scoreMethod: 'zero'})
+      ).then(function(response) {
+        assert(response.columns != null)
+        assert(response.rows != null)
+        for(let i=0; i<response.rows.length; i++){
+          assert(response.rows[i].score.value == 0)
+        }
+        done();
+      }).catch(error => done(error));
+    });
+
+    it('should search documents with random scoring method', function(done) {
+      execPlan(
+          p.fromSearch(p.cts.jsonPropertyValueQuery('instrument', 'trumpet'),
+              ['score', 'quality'], null, { scoreMethod: 'random'})
+      ).then(function(response) {
+        assert(response.columns != null)
+        assert(response.rows != null)
+        done();
+      }).catch(error => done(error));
+    });
   });
 });
