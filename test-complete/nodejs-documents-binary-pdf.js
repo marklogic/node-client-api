1d50e909b948af622edd9ebf6db5fdc3a5c9c161
Steve Biondi <biondi@progress.com>
2025-10-20 10:00:29 -0700

    MLE-24755 binary doc test fixes

--- test-complete/nodejs-documents-binary-pdf.js
+++ test-complete/nodejs-documents-binary-pdf.js
@@ -1,87 +1,72 @@
 /*
 * Copyright (c) 2015-2025 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
 */
-var should = require('should');
+const should = require('should');
 
-var fs     = require('fs');
-var stream = require('stream');
-var util   = require('util');
+const fs     = require('fs');
+const stream = require('stream');
+const util   = require('util');
 
-var concatStream = require('concat-stream');
-var valcheck     = require('core-util-is');
-var testconfig = require('../etc/test-config-qa.js');
+const concatStream = require('concat-stream');
+const valcheck     = require('core-util-is');
+const testconfig = require('../etc/test-config-qa.js');
 
-var marklogic = require('../');
-var q = marklogic.queryBuilder;
+const marklogic = require('../');
+const q = marklogic.queryBuilder;
 
-var dbReader = marklogic.createDatabaseClient(testconfig.restReaderConnection);
-var dbWriter = marklogic.createDatabaseClient(testconfig.restWriterConnection);
-var dbAdmin = marklogic.createDatabaseClient(testconfig.restAdminConnection);
+const dbReader = marklogic.createDatabaseClient(testconfig.restReaderConnection);
+const dbWriter = marklogic.createDatabaseClient(testconfig.restWriterConnection);
+const dbAdmin = marklogic.createDatabaseClient(testconfig.restAdminConnection);
+
 
 describe('Binary documents test', function () {
-    var binaryPath = __dirname + '/data/somePdfFile.pdf';
-    var uri = '/test/binary/somePdfFile.pdf';
-    var binaryValue = null;
-    before(function (done) {
-        this.timeout(10000);
-        fs.createReadStream(binaryPath).
-            pipe(concatStream({ encoding: 'buffer' }, function (value) {
-                binaryValue = value;
-                done();
-            }));
+    const binaryPath = __dirname + '/data/somePdfFile.pdf';
+    let binaryValue = null;
+
+    const uri = '/test/write/somePdfFile.pdf';
+
+    before(async function () {
+        binaryValue = await new Promise((resolve, reject) => {
+            fs.createReadStream(binaryPath)
+                .pipe(concatStream({ encoding: 'buffer' }, function (value) {
+                    resolve(value);
+                }))
+                .on('error', reject);
+        });
     });
 
-    it('should write the binary with Readable stream', function (done) {
-        this.timeout(10000);
-        var uri = '/test/write/somePdfFile.pdf';
-        var readableBinary = new ValueStream(binaryValue);
-        //readableBinary.pause();
-        dbWriter.documents.write({
-            uri: uri,
-            contentType: 'application/pdf',
-            quality: 25,
-            properties: { prop1: 'foo' },
-            content: readableBinary
-        }).
-            result(function (response) {
-                response.should.have.property('documents');
-                done();
-            }, done);
-    });
+    it('should write, read, read metadata, verify and delete the binary pdf content', async function () {
 
-    it('should read the binary with Readable stream', function (done) {
-        this.timeout(10000);
-        var uri = '/test/write/somePdfFile.pdf';
-        dbReader.documents.read(uri).
-            result(function (documents) {
-                //console.log(JSON.stringify(documents, null, 2));
-                JSON.stringify(binaryValue).should.equal(
-                    JSON.stringify(documents[0].content));
-                done();
-            }, done);
-    });
+        const readableBinary = new ValueStream(binaryValue);
+        try {
+            const writeResponse = await dbWriter.documents.write({
+                uri: uri,
+                contentType: 'application/pdf',
+                quality: 25,
+                properties: { prop1: 'foo' },
+                content: readableBinary
+            }).result();
+            writeResponse.should.have.property('documents');
 
-    it('should read the binary document metadata', function (done) {
-        this.timeout(10000);
-        var uri = '/test/write/somePdfFile.pdf';
-        dbReader.documents.read({ uris: uri, categories: ['metadata'] })
-            .result(function (documents) {
-                var document = documents[0];
-                document.quality.should.equal(25);
-                document.properties.prop1.should.equal('foo');
-                done();
-            }, done);
-    });
+            const docReadResp = await dbReader.documents.read(uri).result();
+            docReadResp[0].contentType.should.equal('application/pdf');
+            docReadResp[0].content.length.should.equal(binaryValue.length);
+            Buffer.compare(binaryValue, docReadResp[0].content).should.equal(0);
 
-    it('should delete the pdf file', function (done) {
-        dbAdmin.documents.removeAll({
-            all: true
-        }).
-            result(function (response) {
-                done();
-            }, done);
-    });
+            const docReadRespMeta = await dbReader.documents.read(
+                { uris: uri, categories: ['metadata'] }
+            ).result();
+            docReadRespMeta[0].quality.should.equal(25);
+            docReadRespMeta[0].properties.prop1.should.equal('foo');
 
+            // original test deleted all documents. This one just deletes the one we wrote.
+            await dbAdmin.documents.remove(uri).result();
+
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    });
 });
 
 function ValueStream(value) {
