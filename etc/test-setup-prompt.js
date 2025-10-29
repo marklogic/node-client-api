7045e9107172dbc31a15edb3e7f531f4c3ac0e0e
Rob Rudin <rudin@progress.com>
2025-10-29 09:06:50 -0400

    MLE-24733 Deleting what should be unused etc/ files
    There are no references to these, so deleting these should be fine.

--- etc/test-setup-prompt.js
+++ etc/test-setup-prompt.js
@@ -1,8 +1,6 @@
 /*
 * Copyright (c) 2015-2025 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
 */
-// Simplified version - no more prompting, just returns admin/admin
-// TODO: Remove this file when ml-gradle migration is complete
 
 function promptForAdmin(done) {
   // Always use admin/admin - no prompting needed
